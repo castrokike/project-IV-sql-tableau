92a22ad1b5547cdb001fc6f6cd54927fd55f37fc
Andres Castro <castroandres92@gmail.com>
2023-02-28 11:38:32 +0100

    Commit at: 02/28/23 11:38:32

--- sql_table_editing.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE `friends`.`seasons` 
-CHANGE COLUMN `ep_number_overall` `ep_number_overall` INT NULL DEFAULT NULL ,
-CHANGE COLUMN `ep_number_season` `ep_number_season` INT NULL DEFAULT NULL ,
-CHANGE COLUMN `us_viewers_mm` `us_viewers_mm` DECIMAL(3) NULL DEFAULT NULL ,
-CHANGE COLUMN `rating_1` `rating_1` DECIMAL(3) NULL DEFAULT NULL ,
-CHANGE COLUMN `rating_2` `rating_2` DECIMAL(3) NULL DEFAULT NULL ,
-CHANGE COLUMN `us_viewers_mm_2` `us_viewers_mm_2` DECIMAL(3) NULL DEFAULT NULL ,
-CHANGE COLUMN `season` `season` INT NULL DEFAULT NULL ;
-
